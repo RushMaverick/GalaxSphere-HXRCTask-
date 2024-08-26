75ce102e2cf71c21852532a3e90e0758540e04e4
Rasmus <rasse.rask@gmail.com>
2024-08-26 14:55:36 +0300

    feat: Particle system added to Player when touching trigger for death

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	private Rigidbody2D _rb2d;
-	private float thrust = 3f;
+	private SpriteRenderer _spriteRenderer;
+	private Transform _ps; 
+	private float thrust = 5f;
 
-	void Awake()
-    {
+	//Searches through the parent by the childName and returns the transform for it if it exists.
+	public Transform GetChildByName(Transform parent, string childName) {
+
+		for (int i = 0; i < transform.childCount; i++) {
+			Transform child = transform.GetChild(i);
+			if (child.name == childName)
+				return child;
+		}
+		Debug.LogError($"Child with name {childName} not found.");
+		return null;
+	}
+
+	void Awake() {
         _rb2d = gameObject.GetComponent<Rigidbody2D>();
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		_ps = this.GetChildByName(gameObject.transform, "PlayerDeath");
     }
-    void Start()
-    {
-        
-    }
+	void Start() {
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
+    void Update() {
         if (Input.GetKeyDown("space")){
-			_rb2d.AddForce(transform.up * thrust, ForceMode2D.Impulse);
+			_rb2d.velocity = Vector2.up * thrust;
 			Debug.Log("JUMP");
 		}
-    }
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+			PlayerDeath();
+	}
+
+	void PlayerDeath() {
+		_rb2d.simulated = false;
+		_spriteRenderer.enabled = false;
+		_ps.GetComponent<ParticleSystem>().Play();
+	}
 }
