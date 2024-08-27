f0e695d6d37f6ef8e235274981789ea66d3942e5
Rasmus <rasse.rask@gmail.com>
2024-08-27 11:40:19 +0300

    feat: Change SwitchColor function to randomize the color of the player sprite while comparing it to the previous color.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,13 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 	private Rigidbody2D _rb2d;
+	private int _lastColor;
 	private SpriteRenderer _spriteRenderer;
 	private Transform _ps; 
 	private float thrust = 5f;
 
-	//Searches through the parent by the childName and returns the transform for it if it exists.
-	public Transform GetChildByName(Transform parent, string childName) {
+
+	public Transform GetChildByName(Transform parent, string childName) { //Searches through the parent by the childName and returns the transform for it if it exists.
 
 		for (int i = 0; i < transform.childCount; i++) {
 			Transform child = transform.GetChild(i);
@@ -24,7 +26,9 @@ public class Player : MonoBehaviour
 	void Awake() {
         _rb2d = gameObject.GetComponent<Rigidbody2D>();
         _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		_spriteRenderer.color = Color.red;
 		_ps = this.GetChildByName(gameObject.transform, "PlayerDeath");
+		_lastColor = 0;
     }
 	void Start() {
 	}
@@ -32,7 +36,6 @@ public class Player : MonoBehaviour
     void Update() {
         if (Input.GetKeyDown("space")){
 			_rb2d.velocity = Vector2.up * thrust;
-			Debug.Log("JUMP");
 		}
 	}
 
@@ -44,8 +47,19 @@ public class Player : MonoBehaviour
 	}
 
 	private void SwitchColor(){
-		_spriteRenderer.color = Color.blue;
-		Debug.Log("Switched color!");
+		int randIndex = 0;
+		Color[] colors = new Color[]
+		{
+			Color.red,
+			Color.blue,
+			Color.green,
+			Color.yellow
+		};
+		
+		while (randIndex == _lastColor)
+			randIndex = UnityEngine.Random.Range(0, colors.Length); //This randomizes the color by using Random.Range to randomize a number to pick from the Color array.
+		_lastColor = randIndex; 
+		_spriteRenderer.color = colors[randIndex];
 	}
 
 	private void PlayerDeath() {
